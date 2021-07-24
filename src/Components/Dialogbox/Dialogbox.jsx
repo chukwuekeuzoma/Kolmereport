b0b0f5e549dc2175cfe225a3fa6cf76e17a613d5
mac <chukwuekeuzoma@gmail.com>
2021-07-24 20:41:32 +0100

    dialog box

---
+++ src/Components/Dialogbox/Dialogbox.jsx
@@ -0,0 +1,197 @@
+import React from 'react';
+import { Grid, Paper, TextField, Button, Dialog, Slide, DialogContent, DialogActions } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import "./Dialogbox.scss"
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const useStyles = makeStyles({
+    backDrop: {
+        backdropFilter: "blur(3px)",
+        position:"relative"
+        // backgroundColor:'rgba(0,0,30,0.4)'
+      },
+
+      root: {
+        '& label.Mui-focused': {
+            color: '#cc7722',
+        },
+        '& .MuiInputBase-root': {
+            color: 'black'
+        },
+        '&.MuiTextField-root label': {
+            top: 8,
+            bottom: 8
+        },
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+                borderColor: 'black',
+            },
+            '&:hover': {
+                borderColor: '#cc7722',
+            },
+            '&.Mui-focused fieldset': {
+                borderColor: '#cc7722',
+            },
+
+        },
+    },
+
+})
+
+export default function Dialogbox({OpenBox, CloseBox}) {
+    const classes = useStyles();
+
+    return (
+        <>
+            <div>
+                <Dialog
+                   open={OpenBox}
+                   TransitionComponent={Transition}
+                   keepMounted
+                   onClose={CloseBox}
+                   aria-labelledby="alert-dialog-slide-title"
+                   aria-describedby="alert-dialog-slide-description"
+                   BackdropProps={{
+                    classes: {
+                      root: classes.backDrop,
+                    },
+                    }}
+                >
+                     <DialogActions>
+                        <Button onClick={CloseBox} variant="outlined" >
+                                X
+                        </Button>
+                     </DialogActions>
+                     <DialogContent style={{ width: "420px", height: "auto"}}>
+                         <form className={classes.root}> 
+                            <div className="Dia_field_one_container">
+                                <div>
+                                    <TextField
+                                        size="small"
+                                        label="Name"
+                                        placeholder="Name"
+                                        // id="amount"
+                                        type="name"
+                                        variant="outlined"
+                                        // className="account_textfield"
+                                        // onChange={e => setamount(e.target.value)}
+                                    />
+                                </div>
+                                <div>
+                                    <TextField
+                                        size="small"
+                                        label="Phone"
+                                        placeholder="Phone"
+                                        // id="amount"
+                                        type="phone"
+                                        variant="outlined"
+                                        className="Phone_textfield"
+                                        // onChange={e => setamount(e.target.value)}
+                                    />
+
+                                </div>
+                            </div>
+                            <div>
+                                <TextField
+                                    size="small"
+                                    label="Delivery Address"
+                                    placeholder="Delivery Address"
+                                    // id="amount"
+                                    type="name"
+                                    variant="outlined"
+                                    className="Delivery_textfield"
+                                    // onChange={e => setamount(e.target.value)}
+                                />
+                            </div>
+                            <div>
+                                <TextField
+                                    size="small"
+                                    label="Product Name"
+                                    placeholder="Product Name"
+                                    // id="amount"
+                                    type="name"
+                                    variant="outlined"
+                                    className="Product_textfield"
+                                    // onChange={e => setamount(e.target.value)}
+                                />
+                            </div>
+                            <div>
+                                <TextField
+                                    size="small"
+                                    label="Product description"
+                                    placeholder="Product description"
+                                    // id="amount"
+                                    type="name"
+                                    variant="outlined"
+                                    className="Delivery_textfield"
+                                    // onChange={e => setamount(e.target.value)}
+                                />
+                            </div>
+                            <div className="Dia_field_one_container">
+                                <div>
+                                    <TextField
+                                        size="small"
+                                        label="Quantity"
+                                        placeholder="Quantity"
+                                        // id="amount"
+                                        type="name"
+                                        variant="outlined"
+                                        className="Quantity_textfield"
+                                        // onChange={e => setamount(e.target.value)}
+                                    />
+
+                                </div>
+                                <div>
+                                    <TextField
+                                            size="small"
+                                            label="Weight (Kg)"
+                                            placeholder="Weight (Kg)"
+                                            // id="amount"
+                                            type="name"
+                                            variant="outlined"
+                                            className="Weight_textfield"
+                                            // onChange={e => setamount(e.target.value)}
+                                        />
+
+                                </div>
+                                 <div>
+                                    <TextField
+                                            size="small"
+                                            label="Freight"
+                                            placeholder="Freight"
+                                            // id="amount"
+                                            type="name"
+                                            variant="outlined"
+                                            className="Freight_textfield"
+                                            // onChange={e => setamount(e.target.value)}
+                                        />
+                                </div>
+                            </div>
+                            <div>
+                                <TextField
+                                        size="small"
+                                        label="Cost"
+                                        placeholder="Cost"
+                                        // id="amount"
+                                        type="name"
+                                        variant="outlined"
+                                        className="Cost_textfield"
+                                        // onChange={e => setamount(e.target.value)}
+                                    />
+                            </div>
+                            <Button variant="outlined" className="Dialog_button">
+                                New Order
+                           </Button>
+                         </form>
+
+                     </DialogContent>
+
+                </Dialog>
+
+            </div>
+        </> 
+    );
+}
--- src/Components/Homepage/Hompage.jsx
+++ src/Components/Homepage/Hompage.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react'
 import "./Hompage.scss"
 import {Button} from '@material-ui/core';
 import { DataGrid } from '@material-ui/data-grid';
+import Dialogbox from '../Dialogbox/Dialogbox';
 
 export default function Hompage(props) {
     
-    const columns = [
+    const[Open, setOpen] = useState(false)
+
+    const columns = [ 
         { field: 'id', headerName: "S/N", width: 100 },
         { field: 'requestId', headerName: 'item name', width: 150 },
         { field: 'date', headerName: 'Date', width: 120 },
@@ -15,14 +18,24 @@ export default function Hompage(props) {
     const rows = [
         // { id: 1, requestId: 'none', amount: 'none'},
       ];
+
+      const handleClose = () => {
+        setOpen(false);
+      };
+
+        const handleOpen = () => {
+            setOpen(true);
+        };
+
     return (
         <>
           <div>
+               <Dialogbox OpenBox={Open} CloseBox={handleClose}/>
                 <div className="Hom_container">
                     <div>
                         <span>Orders</span>
                     </div>
-                    <Button variant="outlined" className="Hom_button"> 
+                    <Button variant="outlined" className="Hom_button" onClick={handleOpen}> 
                         New Order
                     </Button>
                 </div>
@@ -32,6 +45,7 @@ export default function Hompage(props) {
                 <div className="Data_grid_container">
                     <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
                 </div>
+                
            </div>
         </>
     );
