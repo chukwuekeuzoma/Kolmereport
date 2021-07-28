d80a0920351101458ba0789619d6297d3a14877a
mac <chukwuekeuzoma@gmail.com>
2021-07-28 14:59:39 +0100

    formlik

--- src/Components/Dialogbox/Dialogbox.jsx
+++ src/Components/Dialogbox/Dialogbox.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react'
 import {TextField, Button, Dialog, Slide, DialogContent, DialogActions } from '@material-ui/core';
+import { useFormik } from "formik"
+import * as yup from 'yup';
+import Alert from '@material-ui/lab/Alert';
+import axios from "axios"
 import { makeStyles } from '@material-ui/core/styles';
+import PulseLoader from "react-spinners/ClipLoader"
 import "./Dialogbox.scss"
 
 const Transition = React.forwardRef(function Transition(props, ref) {
@@ -42,8 +47,27 @@ const useStyles = makeStyles({
 
 })
 
+const validationSchema = yup.object({
+    name: yup.string().min(2, "Enter your real name").required("Name required"),
+    phone: yup.string().min(11, "Phone required").required("Phone required"),
+    // email: yup.string().email("Pls enter a valid email address").required(),
+    address: yup.string().required("Address required"),
+    productName: yup.string().required("Product required"),
+    description: yup.string().required("Description required"),
+    quantity: yup.string().required("Quantity required"),
+    weight: yup.string().required("Weight required"),
+    freight: yup.string().required("Freight required"),
+    cost: yup.string().required("Cost required"),
+
+  });
+
 export default function Dialogbox({OpenBox, CloseBox}) {
     const classes = useStyles();
+   
+    const [error, setError] = useState("")
+    const [success, setSuccess] = useState("")
+    const [Loader, setLoader] = useState(false)
+
 
     // Adding close on escape for keyboard users.
     document.addEventListener("keydown", function (event) {
@@ -52,6 +76,49 @@ export default function Dialogbox({OpenBox, CloseBox}) {
         }
       });
 
+
+      const onSubmit = async (values) => {
+        //    setLoader(true)
+        // axios.post('https://delivered-demo.herokuapp.com/api/orders/process', values)
+        //   .then(response => {
+        //     if (response.data.status === "success") {
+        //       setSuccess(response.data.message)
+        //       setError("")
+        //     };
+        //     if (response.data.data.status === "error") {
+        //       setError(response.data.message)
+        //       setSuccess("")
+        //     }
+        //     formik.resetForm();
+        //     setLoader(false)
+        //     console.log(data.data.message)
+        //   })
+        //   .catch((error) => {
+        //     setError(error.response.data.message)
+        //     setSuccess("")
+        //     setLoader(false)
+        //     console.error('Error:', error);
+        //   });
+      }
+
+
+      const formik = useFormik({
+        initialValues: {
+          name: "",
+          phone: "",
+          address: "",
+          productName: "",
+          description: "",
+          quantity: "",
+          weight:"",
+          freight:"",
+          cost: "",
+        },
+        onSubmit, 
+        validateOnBlur: true,
+        validationSchema: validationSchema,
+    
+      });
     return (
         <>
             <div>
@@ -74,125 +141,208 @@ export default function Dialogbox({OpenBox, CloseBox}) {
                         </Button>
                      </DialogActions>
                      <DialogContent style={{ width: "420px", height: "auto"}}>
-                         <form className={classes.root}> 
+                         <div className="Alert">
+                            {error && <Alert severity="error">{error}</Alert>}
+                            {success && <Alert severity="success">{success}</Alert>}
+                          </div>
+                         <form className={classes.root} onSubmit={formik.handleSubmit}> 
                             <div className="Dia_field_one_container">
                                 <div>
-                                    <TextField
-                                        size="small"
-                                        label="Name"
-                                        placeholder="Name"
-                                        // id="amount"
-                                        type="name"
-                                        variant="outlined"
-                                        // className="account_textfield"
-                                        // onChange={e => setamount(e.target.value)}
-                                    />
-                                </div>
-                                <div>
-                                    <TextField
-                                        size="small"
-                                        label="Phone"
-                                        placeholder="Phone"
-                                        // id="amount"
-                                        type="phone"
-                                        variant="outlined"
-                                        className="Phone_textfield"
-                                        // onChange={e => setamount(e.target.value)}
-                                    />
+                                    <div>
+                                        <TextField
+                                            size="small"
+                                            label="Name"
+                                            placeholder="Name"
+                                            name="name"
+                                            type="name"
+                                            variant="outlined"
+                                            value={formik.values.name}
+                                            // className="account_textfield"
+                                            onChange={formik.handleChange}
+                                            onBlur={formik.handleBlur}
+                                        />
+                                    </div>
+                                    <div className="field_container">
+                                        <span className="field">
+                                            {formik.touched.name && formik.errors.name ? formik.errors.name : ""}
+                                        </span>
+                                    </div>
+                               </div>
+                               <div>
+                                    <div>
+                                        <TextField
+                                            size="small"
+                                            label="Phone"
+                                            placeholder="Phone"
+                                            name="phone"
+                                            type="phone"
+                                            variant="outlined"
+                                            className="Phone_textfield"
+                                            value={formik.values.phone}
+                                            onChange={formik.handleChange}
+                                            onBlur={formik.handleBlur}
+                                        />
 
-                                </div>
+                                    </div>
+                                    <div className="field_container">
+                                        <span className="field">
+                                            {formik.touched.phone && formik.errors.phone? formik.errors.phone : ""}
+                                        </span>
+                                    </div>
+                               </div>
                             </div>
                             <div>
                                 <TextField
                                     size="small"
                                     label="Delivery Address"
                                     placeholder="Delivery Address"
-                                    // id="amount"
+                                    name="address"
                                     type="name"
                                     variant="outlined"
                                     className="Delivery_textfield"
-                                    // onChange={e => setamount(e.target.value)}
+                                    value={formik.values.address}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
                                 />
+                            </div>
+                            <div className="field_container">
+                                    <span className="field">
+                                        {formik.touched.address && formik.errors.address? formik.errors.address : ""}
+                                    </span>
                             </div>
                             <div>
                                 <TextField
                                     size="small"
                                     label="Product Name"
                                     placeholder="Product Name"
-                                    // id="amount"
+                                    name="productName"
                                     type="name"
                                     variant="outlined"
                                     className="Product_textfield"
-                                    // onChange={e => setamount(e.target.value)}
+                                    value={formik.values.productName}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
                                 />
+                            </div>
+                            <div className="field_container">
+                                    <span className="field">
+                                        {formik.touched.productName && formik.errors.productName? formik.errors.productName : ""}
+                                    </span>
                             </div>
                             <div>
                                 <TextField
                                     size="small"
                                     label="Product description"
                                     placeholder="Product description"
-                                    // id="amount"
+                                    name="description"
                                     type="name"
                                     variant="outlined"
                                     className="Delivery_textfield"
-                                    // onChange={e => setamount(e.target.value)}
+                                    value={formik.values.description}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
                                 />
+                            </div>
+                            <div className="field_container">
+                                    <span className="field">
+                                        {formik.touched.description && formik.errors.description? formik.errors.description : ""}
+                                    </span>
                             </div>
                             <div className="Dia_field_one_container">
                                 <div>
-                                    <TextField
-                                        size="small"
-                                        label="Quantity"
-                                        placeholder="Quantity"
-                                        // id="amount"
-                                        type="name"
-                                        variant="outlined"
-                                        className="Quantity_textfield"
-                                        // onChange={e => setamount(e.target.value)}
-                                    />
+                                    <div>
+                                        <TextField
+                                            size="small"
+                                            label="Quantity"
+                                            placeholder="Quantity"
+                                            name="quantity"
+                                            type="phone"
+                                            variant="outlined"
+                                            className="Quantity_textfield"
+                                            value={formik.values.quantity}
+                                            onChange={formik.handleChange}
+                                            onBlur={formik.handleBlur}
+                                        />
 
+                                    </div>
+                                    <div className="field_container">
+                                        <span className="field">
+                                            {formik.touched.quantity && formik.errors.quantity? formik.errors.quantity : ""}
+                                        </span>
+                                    </div>
+                                 </div>
+                                 <div>
+                                    <div>
+                                        <TextField
+                                                size="small"
+                                                label="Weight (Kg)"
+                                                placeholder="Weight (Kg)"
+                                                name="weight"
+                                                type="name"
+                                                variant="outlined"
+                                                className="Weight_textfield"
+                                                value={formik.values.weight}
+                                                onChange={formik.handleChange}
+                                                onBlur={formik.handleBlur}
+                                            />
+
+                                    </div>
+                                    <div className="field_container">
+                                        <span className="field">
+                                            {formik.touched.weight && formik.errors.weight? formik.errors.weight : ""}
+                                        </span>
+                                    </div>
                                 </div>
                                 <div>
-                                    <TextField
-                                            size="small"
-                                            label="Weight (Kg)"
-                                            placeholder="Weight (Kg)"
-                                            // id="amount"
-                                            type="name"
-                                            variant="outlined"
-                                            className="Weight_textfield"
-                                            // onChange={e => setamount(e.target.value)}
-                                        />
-
-                                </div>
-                                 <div>
-                                    <TextField
-                                            size="small"
-                                            label="Freight"
-                                            placeholder="Freight"
-                                            // id="amount"
-                                            type="name"
-                                            variant="outlined"
-                                            className="Freight_textfield"
-                                            // onChange={e => setamount(e.target.value)}
-                                        />
-                                </div>
+                                    <div>
+                                        <TextField
+                                                size="small"
+                                                label="Freight"
+                                                placeholder="Freight"
+                                                name="freight"
+                                                type="phone"
+                                                variant="outlined"
+                                                className="Freight_textfield"
+                                                value={formik.values.freight}
+                                                onChange={formik.handleChange}
+                                                onBlur={formik.handleBlur}
+                                            />
+                                    </div>
+                                    <div className="field_container">
+                                        <span className="field">
+                                            {formik.touched.freight && formik.errors.freight? formik.errors.freight : ""}
+                                        </span>
+                                    </div>
+                                 </div>
                             </div>
                             <div>
                                 <TextField
                                         size="small"
                                         label="Cost"
                                         placeholder="Cost"
-                                        // id="amount"
-                                        type="name"
+                                        name="cost"
+                                        type="phone"
                                         variant="outlined"
                                         className="Cost_textfield"
-                                        // onChange={e => setamount(e.target.value)}
+                                        value={formik.values.cost}
+                                        onChange={formik.handleChange}
+                                        onBlur={formik.handleBlur}
                                     />
                             </div>
-                            <Button variant="outlined" className="Dialog_button">
-                                Submit
-                           </Button>
+                            <div className="field_container">
+                                    <span className="field">
+                                        {formik.touched.cost && formik.errors.cost? formik.errors.cost : ""}
+                                    </span>
+                             </div>
+                             {Loader?
+                                <Button variant="outlined" className="Dialog_button">
+                                    <PulseLoader color={"white"} size={30}/>
+                                </Button>
+                                :
+                                <Button variant="outlined" className="Dialog_button" type="submit" disabled={!formik.isValid}>
+                                    Submit
+                                </Button>
+                             }
                          </form>
 
                      </DialogContent>
--- src/Components/Homepage/Hompage.jsx
+++ src/Components/Homepage/Hompage.jsx
@@ -1,24 +1,49 @@
 import React, { useState } from 'react'
 import "./Hompage.scss"
 import {Button} from '@material-ui/core';
-import { DataGrid } from '@material-ui/data-grid';
+// import { DataGrid } from '@material-ui/data-grid';
 import Dialogbox from '../Dialogbox/Dialogbox';
 import Fade from 'react-reveal/Fade'
+import {Table, TableBody, TableCell,TableContainer,TableHead,TableRow, Paper, makeStyles} from '@material-ui/core';
+
+
+const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+  });
+
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+  
+  const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+  ];
 
 export default function Hompage(props) {
-    
+     const classes = useStyles();
     const[Open, setOpen] = useState(false)
 
-    const columns = [ 
-        { field: 'id', headerName: "S/N", width: 100 },
-        { field: 'requestId', headerName: 'item name', width: 150 },
-        { field: 'date', headerName: 'Date', width: 120 },
-        { field: 'status', headerName: 'satus', width: 120 },
-    ]
 
-    const rows = [
-        // { id: 1, requestId: 'none', amount: 'none'},
-      ];
+    // const columns = [ 
+    //     { field: 'id', headerName: "S/N", width: 100 },
+    //     { field: 'requestId', headerName: 'item name', width: 150 },
+    //     { field: 'date', headerName: 'Date', width: 120 },
+    //     { field: 'status', headerName: 'satus', width: 120 },
+    // ]
+
+    // const rows = [
+    //     { id: 1, requestId: 'none', date: 'none' , status:'none'},
+    //     { id: 2, requestId: 'Martins', date: '20/21/1995' , status:'pending'},
+    //     { id: 3, requestId: 'Henry', date: '20/21/1996' , status:'susessful'},
+    //     { id: 4, requestId: 'Chisom', date: '20/21/1997' , status:'none'},
+    //     { id: 5, requestId: 'none', date: 'none' , status:'none'},
+    //   ];
 
       const handleClose = () => {
         setOpen(false);
@@ -45,7 +70,36 @@ export default function Hompage(props) {
                    <div className="Hom_line"></div>
                 </div>
                 <div className="Data_grid_container">
-                    <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
+                    {/* <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection /> */}
+                    <TableContainer component={Paper}>
+                        <Table className={classes.table} aria-label="simple table">
+                            <TableHead>
+                            <TableRow>
+                                <TableCell>Dessert (100g serving)</TableCell>
+                                <TableCell align="right">Calories</TableCell>
+                                <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                            </TableRow>
+                            </TableHead>
+                            <TableBody>
+                            {rows.map((row) => (
+                                // <a href="row.link" style={{textDecoration:"none"}}>
+
+                                <TableRow key={row.name}>
+                                <TableCell component="th" scope="row">
+                                    {row.name}
+                                </TableCell>
+                                <TableCell align="right">{row.calories}</TableCell>
+                                <TableCell align="right">{row.fat}</TableCell>
+                                <TableCell align="right">{row.carbs}</TableCell>
+                                <TableCell align="right">{row.protein}</TableCell>
+                                </TableRow>
+                                // </a>
+                            ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
                 </div>
                 
            </div>
